73dda69ad5c34c3a537c5566e8ef572239932ef9
subhamroy007 <60667175+subhamroy007@users.noreply.github.com>
2021-11-13 23:01:55 +0530

    Update MessageText.tsx

--- SOCIALWEB/components/MessageText.tsx
+++ SOCIALWEB/components/MessageText.tsx
@@ -14,7 +14,6 @@ export interface MessageTestProps {
 }
 const MessageText = ({ message, style }: MessageTestProps) => {
 
-    const msg = 'hello i am subrata bid ekfbbekg iengkergueg begekgbieg  hbubir ebgbiug brriebiuu uegbigbigb  !!'
     return (
         <SafeAreaView style={styles.Container}>
             <Text style={styles.textContainer}>{message}</Text>
@@ -32,7 +31,8 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderRadius: 20,
         padding: 3,
-        backgroundColor: 'skyblue'
+        //didnot get text background color 
+        backgroundColor: '#EBE8FB'
 
     }
 })
