c859ab270ec6f2c5e771bce714d532cf7f9ef6ec
subhamroy007 <subhamroy199907@gmail.com>
2021-11-21 14:46:09 +0530

    chat message component added

--- SOCIALWEB/components/MessageText.tsx
+++ SOCIALWEB/components/MessageText.tsx
@@ -1,40 +1,35 @@
 import React from "react";
 import {
-    SafeAreaView,
-    StyleSheet,
-    Text,
-    ViewStyle,
-    StyleProp
-
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  ViewStyle,
+  StyleProp,
 } from "react-native";
 //import { SafeAreaView } from "react-native-safe-area-context";
 export interface MessageTestProps {
-    message: string;
-    style?: StyleProp<ViewStyle>;
+  message: string;
+  style?: StyleProp<ViewStyle>;
 }
 const MessageText = ({ message, style }: MessageTestProps) => {
-
-    return (
-        <SafeAreaView style={styles.Container}>
-            <Text style={styles.textContainer}>{message}</Text>
-
-        </SafeAreaView>
-    )
-
-}
+  return (
+    <SafeAreaView style={styles.Container}>
+      <Text style={styles.textContainer}>{message}</Text>
+    </SafeAreaView>
+  );
+};
 const styles = StyleSheet.create({
-    Container: {
-        borderRadius: 20,
-        alignSelf: 'flex-start'
-    },
-    textContainer: {
-        borderWidth: 1,
-        borderRadius: 20,
-        padding: 3,
-        //didnot get text background color 
-        backgroundColor: '#EBE8FB'
-
-    }
-})
+  Container: {
+    borderRadius: 20,
+    alignSelf: "flex-start",
+  },
+  textContainer: {
+    borderWidth: 1,
+    borderRadius: 20,
+    padding: 3,
+    //didnot get text background color
+    backgroundColor: "#EBE8FB",
+  },
+});
 
 export default MessageText;
